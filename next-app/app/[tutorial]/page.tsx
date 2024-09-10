fb062c57605b9c7ae8e7d240e461ffafb77ba2e4
Ankur Seth <iankurseth@gmail.com>
2024-09-10 22:29:37 +0530

    Implemented generateMetadata() on the only dynamic route

--- next-app/app/[tutorial]/page.tsx
+++ next-app/app/[tutorial]/page.tsx
@@ -3,6 +3,14 @@ import { tutorials } from "@/data";
 
 
 
+export async function generateMetadata ({ params }: { params: { tutorial: string } }) {
+	const tutorial = tutorials.find(t => t.name === params.tutorial) || tutorials[0];
+	return {
+		title: `Tutorial on ${tutorial.name}`,
+		description: `Just a tutorial on ${tutorial.name}`
+	};
+}
+
 export async function generateStaticParams () {
 	return tutorials.map(t => ({
 		tutorial: t.name
