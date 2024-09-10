65d55c9ca6ef7d0afd147d89ce07add166f931fd
Ankur Seth <iankurseth@gmail.com>
2024-09-10 18:11:19 +0530

    Added empty components, data and utils modules

---
+++ next-app/data/data.ts
@@ -0,0 +1,14 @@
+// import snippetsJson from '../snippets.json';
+
+
+
+export interface SnippetDS {
+	name: string,
+	html: string,
+	css: string,
+	meta: {
+		heading: string
+	}
+}
+
+// export const snippets: SnippetDS[] = snippetsJson.snippets;
