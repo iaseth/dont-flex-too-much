f946e483cb90b790e2bf5a69459dac05e468bea4
Ankur Seth <iankurseth@gmail.com>
2024-09-10 22:01:20 +0530

    Added styles for each snippet

--- next-app/components/Tutorial/Snippet/Snippet.tsx
+++ next-app/components/Tutorial/Snippet/Snippet.tsx
@@ -1,4 +1,4 @@
-import { SnippetDS } from "../../data";
+import { SnippetDS } from "../../../data";
 import HighlightedCodeBlock from "./HighlightedCodeBlock/HighlightedCodeBlock";
 
 
@@ -10,20 +10,19 @@ interface SnippetProps {
 export default function Snippet ({
 	snippet
 }: SnippetProps) {
-	const target = `${snippet.name}-section`;
-
 	return (
-		<section id={target} className="bg-gradient-to-b from-zinc-900 via-zinc-950/50 to-zinc-900 py-6">
+		<section id={snippet.className} className="bg-gradient-to-b from-zinc-900 via-zinc-950/50 to-zinc-900 py-6">
 			<header className="container-sm text-left py-6">
 				<h1 className="H4">
-					<a className="mr-4 opacity-30" href={`#${target}`}>#</a>
+					<a className="mr-4 opacity-30" href={`#${snippet.className}`}>#</a>
 					<span>{snippet.meta.heading}</span>
 				</h1>
 				<p>This is just come text. This is just come text. This is just come text. This is just come text. This is just come text. This is just come text. This is just come text. This is just come text.</p>
 			</header>
 
 			<section className="container-lg font-mono grid gap-3 lg:grid-cols-3 py-6">
-				<section id={snippet.name} className="px-3 py-3 bg-zinc-950 border border-zinc-700" dangerouslySetInnerHTML={{__html: snippet.html}}></section>
+				<section id={snippet.name} className={`${snippet.className} px-3 py-3 bg-zinc-950 border border-zinc-700`} dangerouslySetInnerHTML={{__html: snippet.html}}></section>
+				<style dangerouslySetInnerHTML={{__html: snippet.styles}}></style>
 
 				<HighlightedCodeBlock heading="HTML" language="xml" code={snippet.html} />
 				<HighlightedCodeBlock heading="CSS" language="css" code={snippet.css} />
