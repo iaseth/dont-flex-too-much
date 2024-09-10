b642539d891abdc2e0827364b0063a76c0d29018
Ankur Seth <iankurseth@gmail.com>
2024-09-10 15:02:24 +0530

    Added types and now using Light build

--- src/App/components/Snippet/HighlightedCodeBlock/HighlightedCodeBlock.tsx
+++ src/App/components/Snippet/HighlightedCodeBlock/HighlightedCodeBlock.tsx
@@ -1,7 +1,12 @@
-import SyntaxHighlighter from 'react-syntax-highlighter';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import xml from 'react-syntax-highlighter/dist/esm/languages/hljs/xml';
+import css from 'react-syntax-highlighter/dist/esm/languages/hljs/css';
 import { stackoverflowDark as codeStyle } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 
+
+SyntaxHighlighter.registerLanguage('xml', xml);
+SyntaxHighlighter.registerLanguage('css', css);
 
 interface HighlightedCodeBlockProps {
 	code: string,
--- src/App/components/Snippet/Snippet.tsx
+++ src/App/components/Snippet/Snippet.tsx
@@ -19,7 +19,7 @@ export default function Snippet ({
 			<section className="container font-mono grid gap-3 lg:grid-cols-3">
 				<section id={snippet.name} className="px-2 py-2 bg-zinc-950 border border-pink-500" dangerouslySetInnerHTML={{__html: snippet.html}}></section>
 
-				<HighlightedCodeBlock language="html" code={snippet.html} />
+				<HighlightedCodeBlock language="xml" code={snippet.html} />
 				<HighlightedCodeBlock language="css" code={snippet.css} />
 			</section>
 		</section>
