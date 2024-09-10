652f8479427afe8a52b331eafc06532c91295c7f
Ankur Seth <iankurseth@gmail.com>
2024-09-10 10:57:10 +0530

    Cleanen up the defaults

--- src/App/components/Header/Header.tsx
+++ src/App/components/Header/Header.tsx
@@ -7,8 +7,9 @@ interface HeaderProps {
 
 export default function Header ({}: HeaderProps) {
 	return (
-		<header className="bg-blue-500 text-white px-2 py-5 text-center">
-			<h2>Start</h2>
+		<header className="bg-blue-500 text-white px-2 py-8 text-center">
+			<h2>Dont Flex Too Much</h2>
+			<p>A tutorial on CSS Flexbox and Grid.</p>
 		</header>
 	);
 }
--- src/App/pages/MainPage/MainPage.tsx
+++ src/App/pages/MainPage/MainPage.tsx
@@ -1,4 +1,3 @@
-import { RedIcon } from "redicons";
 import { Footer, Header } from "../../components";
 
 
@@ -10,11 +9,7 @@ export default function MainPage () {
 
 			<main className="min-h-screen px-4 py-12">
 				<section className="">
-					<a href="https://github.com/iaseth/vite-react-ts-starter" className="block w-64 m-auto bg-zinc-800 text-white text-center px-8 py-6 rounded">
-						<RedIcon name="bs-foo" className="mx-auto" size={40} />
-						<h1 className="H4">Starter</h1>
-						<h4>vite-react-ts-starter</h4>
-					</a>
+					<h4>dont-flex-too-much</h4>
 				</section>
 			</main>
 
